2c6af44a9664dffc6612546096650652ab938986
Sneezycheesy <j.niksnl@outlook.com>
2025-06-27 10:05:32 +0200

    Initial setup installation script and requirements

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+# Install all required packages that this configuration uses.
+# To add or remove packages, update requirements.txt.
+# This install script assumes use of an ARCH-based distro with the use of Yet Another Yogurt (yay).
+
+# Make sure yay is installed first.
+# The "extra" repository source is needed in Arch.
+if [[ -z $(pacman -Qe | grep yay) ]]; then
+    sudo pacman -S --needed git base-devel go
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd ..
+    rm -rf yay
+fi
+
+packages=""
+for line in $(cat ./requirements.txt); do
+    packages="$packages $line"
+done
+yay -S $packages
