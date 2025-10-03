162621b5a1899e8e6e3db6d47e39830308dd3d65
Sneezycheesy <j.niksnl@outlook.com>
2025-10-03 21:02:41 +0200

    Add update script and start on install script - also safeguard xmonad config

--- install.sh
+++ install.sh
@@ -1,30 +1,45 @@
 #!/usr/bin/bash
-# Install all required packages that this configuration uses.
-# To add or remove packages, update requirements.txt.
-# This install script assumes use of an ARCH-based distro with the use of Yet Another Yogurt (yay).
-
-# Make sure yay is installed first.
-# The "extra" repository source is needed in Arch.
-if [[ -z $(pacman -Qe | grep "yay") ]]; then
-    sudo pacman -S --needed git base-devel go
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-    cd ..
-    rm -rf yay
+##########################################################################
+##########################################################################
+# Install packages based on prefered use cases
+# Always use Yet Another Yaourt (YaY)
+##########################################################################
+##########################################################################
+trap "echo 'Cleaning up'; cd ..; rm -rf yay; exit" SIGINT
+if [[ -z $(command -v yay) ]]; then
+  command -v git || sudo pacman -S git
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  command -v go || sudo pacman -S go
+  makepkg
+  sudo pacman -U yay*.pkg.tar.zst
+  cd ..
+  rm -rf yay
 fi
 
-if [[ -z $(pacman -Qe | grep "yay") ]]; then
-    echo "Yay was not installed correctly, please fix the issue"
-    exit
-fi
+install_packages() {
+  #
+  # When profiles are requested
+  # Only install those
+  #
+  if [[ ! -z $@ ]]; then
+    package_profiles=""
+    for profile in $@; do
+      package_profiles="${package_profiles} ${profile}"
+    done
+  else
+      package_profiles=$(ls ./packages)
+  fi
+    
+  packages=""
+  for package_file in $package_profiles; do
+    packages=$(cat ./packages/${package_file})
+    yay_packages=""
+    for package in $packages; do
+      yay_packages="${yay_packages} ${package}"
+    done
+    yay -S $packages
+  done
+}
 
-packages=""
-for line in $(cat ./requirements.txt); do
-    packages="$packages $line"
-done
-yay -S $packages
-
-if [[ ! -z $(pacman -Qe | grep "xmonad") ]]; do
-    xmonad --recompile
-fi
+install_packages $@
