e90ab15d045ea8f476b53712d0ac0061bf0cae61
Sneezycheesy <j.niksnl@outlook.com>
2026-01-03 12:59:51 +0100

    updated wivrn script to close lutris wrapper https://listen.181fm.com/181-awesome80s_128k.mp3

--- config/.scripts/start_steam_game_with_vr.sh
+++ config/.scripts/start_steam_game_with_vr.sh
@@ -6,16 +6,34 @@
 # Then launch Steam, with the game.
 function start_steam() {
   game_id=${1,,}
+  if [ ! -z `pgrep -x steam` ]; then
+    steam_running=TRUE
+#    pkill -x steam
+    while [ ! -z $(pgrep -x steam) ]; do
+      echo "Closing steam..."
+      sleep 2
+    done
+  fi
   
   # Start WiVRN for the OpenXR connection.
   [[ ! -z `pgrep -if envision` ]] && pkill -if envision
-  [[ ! -z `pgrep -if wivrn` ]] && pkill -if wivrn 
+  [[ ! -z `pgrep -if wivrn` ]] && pkill -if wivrn
+  corectrl & 
   envision --start &
+
+  while [[ -z `pgrep wivrn` ]]; do
+    sleep 2
+  done
   
   while [[ -z `ss -tun | grep :9757` ]]; do
     echo "No WiVRN connection active"
+    # Exit script is wivrn is closed.
+    if [[ -z $(pgrep wivrn) ]]; then
+      kill_steam
+    fi
     sleep 5
   done
+  echo "VR connected"
   # Set audio input and output to WiVRN device.
   sh ~/.scripts/volume.sh sink vr wivr;
   sh ~/.scripts/volume.sh source vr wivrn;
@@ -25,8 +43,11 @@ function start_steam() {
   # From missing the active connection.
   sleep 2
   
-  steam -silent -applaunch ${game_id} &
+  echo "Starting Steam game"
+  setxkbmap us
+  steam-native -silent -applaunch ${game_id} &
   wait_for_game_exit
+
 }
 
 ###
@@ -38,14 +59,17 @@ function wait_for_game_exit() {
   # Waits until steam is no longer running any games, then closes it.
   # Waits for steam before checking for a game to be running.
   while [ -z $(pgrep -x steam) ]; do
+    echo "Steam not running yet"
     sleep 5
   done
   # While a game isn't running yet, do nothing.
   while [ -z $(pgrep -x reaper) ]; do
+    echo "Game not running yet"
     sleep 10
   done
   # Once the game is running, wait for it to close.
   while [ ! -z $(pgrep -x reaper) ]; do
+    echo "Game now running"
     sleep 5 
   done
 
@@ -59,11 +83,14 @@ function wait_for_game_exit() {
 ###
 function kill_steam() {
   # Close everything once the game is done.
-  pkill reaper
+  #pkill reaper
   pkill steam
   pkill envision
   pkill wivrn-server
-  
+  setxkbmap us -variant alt-intl
+  echo $steam_running
+  [[ ! -z $(pgrep lutris-wrapper) ]] && pkill lutris-wrapper 
+  [ $steam_running ] && steam -silent
   exit
 }
 
